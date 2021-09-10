80ee1be0dbf1782b8c1995201b46c659128afb72
Tareq Rahman <tareq7499@gmail.com>
2021-09-10 22:22:26 +0600

    server done, client start. home push friday

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+
+const app = express();
+
+const db = require("./models");
+// db.sequelize.sync();
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
+
+var corsOptions = {
+	origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
