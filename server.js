f9511eb975654e86717d6fa5cad29eafc2ab5bbe
tareq75 <tareq7500@gmail.com>
2022-08-05 13:19:08 +0600

    git commit check

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to my application." });
 });
 
 const PORT = process.env.PORT || 8080;
